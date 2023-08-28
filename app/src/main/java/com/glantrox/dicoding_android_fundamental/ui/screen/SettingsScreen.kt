12e03443bab16c848302884b65eb90910cb029f7
Glantrox <59131023+Izan2020@users.noreply.github.com>
2023-08-28 15:02:02 +0700

    + Updated: Settings Topbar changed to Material 3

--- app/src/main/java/com/glantrox/dicoding_android_fundamental/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/glantrox/dicoding_android_fundamental/ui/screen/SettingsScreen.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.IconButton
 import androidx.compose.material.Switch
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Card
